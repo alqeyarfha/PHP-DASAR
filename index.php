3ccf4e0e457628cba5197d43eb432dfd41e8400c
Alqe <alqeyarfha09@gmail.com>
2025-09-01 10:42:44 +0700

    buat file index

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DASAR</title>
+</head>
+<body>
+    <?php $nama = "candra"?>
+    <h1>selamat datng,<?php echo $nama ?>!</h1>
+</body>
+</html>
