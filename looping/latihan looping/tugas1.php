fcd9f312817fe0a75b3d0449908924f3a135c247
Alqe <alqeyarfha09@gmail.com>
2025-09-02 14:34:54 +0700

    soal loopping

--- looping/latihan looping/tugas1.php
+++ looping/latihan looping/tugas1.php
@@ -1,1 +1,22 @@
-    
+<?php
+$siswa = [
+    "Andi"  => 80,
+    "Budi"  => 70,
+    "Citra" => 90,
+    "Dedi"  => 60,
+    "Eka"   => 75,
+];
+
+echo "Hasil Kelulusan Siswa:";
+
+foreach ($siswa as $nama => $nilai) {
+    if ($nilai >= 75) {
+        echo "<br>";
+
+        echo "- $nama: Lulus (Nilai: $nilai)";
+    } else {
+        echo "<br>";
+
+        echo "- $nama: Tidak Lulus (Nilai: $nilai)";
+    }
+}
